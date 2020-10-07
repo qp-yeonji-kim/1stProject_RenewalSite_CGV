dbd53fdb4c0ffbe9dbdaa5c62b6b12ba1f33f8de
yeon ji_kim <60783510+qp-yeonji-kim@users.noreply.github.com>
2020-10-07 10:25:21 +0900

    index-nav
    2depth에 100% 검정색바가 생기는 제이쿼리 선택자 추가 기능 삽입

--- js/common.js
+++ js/common.js
@@ -1,5 +1,6 @@
 $(document).ready(function () {
 	//1. gnb
+	var $header=$("#header");
 	var $gnb=$("#gnb > ul");
 			
 	$gnb.find("li ul").hide();	//depth2의 ul 태그는 자동으로 숨기고 시작
@@ -10,12 +11,14 @@ $(document).ready(function () {
 		$gnb.find("> li.on").removeClass("on").children("ul").hide();
 		//현재설정
 		$(this).next().show().parent().addClass("on");
+		$header.addClass('active'); //#header 너비의 100% 흰색바 생성을 위한 클래스명 추가
 	});
 
 	//2)nav에서 마우스 떠나기:mouseleave
 	$gnb.on("mouseleave",function  () {
 		//초기화
 		$gnb.find("> li.on").removeClass("on").children("ul").hide();
+		$header.removeClass('active');
 	});
 
 	//3)blur: shift탭을 눌러서 gnb에서 포커스가 나가던지, 탭을 눌러 gnb에서 포커스가 나가던지, 
