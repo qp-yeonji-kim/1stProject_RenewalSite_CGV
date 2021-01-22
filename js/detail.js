e11f892b7cb9df3516e0b88cf2fdf8f0effb2650
yeon ji_kim <60783510+qp-yeonji-kim@users.noreply.github.com>
2021-01-22 23:36:33 +0900

    detail-modal기능 수정

--- js/detail.js
+++ js/detail.js
@@ -3,18 +3,15 @@ $(document).ready(function () {
   $('#stealcut ul li img').on('click', function () {
     // 변수
     var $mdCut = $('#modal_stealcut');
-    var $first = $mdCut.children('.first');
-    var $last = $mdCut.children('.last');
-    var $closeBtn = $mdCut.children('.md_close_btn');
+    var $prevBtn = $mdCut.children('.prev');
+    var $nextBtn = $mdCut.children('.next');
+    var $clsBtn = $mdCut.children('.md_close_btn');
     var $clickImg = $(this);
     var imgNum = $clickImg.parent().index() + 1;
     var imgSrc = '';
 
-    // 클릭하면 바꿀 모달 속성
-    $mdCut.siblings().attr({
-      'aria-hidden': true,
-      inert: true
-    });
+    // 클릭하면 바꿀 모달 접근성
+    $mdCut.siblings().attr({'aria-hidden': true, inert: true});
 
     // dim생성 및 cnt노출
     $mdCut.before('<div id="dim"></div>');
@@ -22,63 +19,85 @@ $(document).ready(function () {
     imgSrc += '../../images/detail/stealcut' + imgNum + '.jpg'; 
 
     $dim.stop().fadeIn('fast').next().css('visibility', 'visible');
-    $('#modal_stealcut img').attr('src', imgSrc);
-    $first.focus();
+    $('#modal_stealcut > img').attr('src', imgSrc);
+    $prevBtn.focus();
 
     if(imgNum === 1) {
-      $('#modal_stealcut .next').focus().prev().attr({'disabled': true}).css({'color': '#7d7d7d'});
+      $nextBtn.focus().prev().attr({'disabled': true}).css({'color': '#7d7d7d'});
+      $nextBtn.on('keydown', function(e){
+        if(e.shiftKey && e.keyCode === 9) {
+          e.preventDefault();
+          $clsBtn.focus();
+        }
+      });
+      $clsBtn.on('keydown', function(e){
+        if(!e.shiftKey && e.keyCode === 9) {
+          e.preventDefault();
+          $nextBtn.focus();
+        }
+      });
+      
     } else if(imgNum === 23) {
-      $('#modal_stealcut .prev').focus().next().attr({'disabled': true}).css({'color': '#7d7d7d'});
+      $prevBtn.focus().next().attr({'disabled': true}).css({'color': '#7d7d7d'});
     } else if(imgNum !== 1 && imgNum !== 23) {
-      $('#modal_stealcut .next, #modal_stealcut .prev').attr({'disabled': false}).css({'color': '#000'});
+      $('#modal_stealcut .prev_next').attr({'disabled': false}).css({'color': '#000'});
     }
-
-    // 다음버튼, 이전버튼
-    $('#modal_stealcut .prev_next').on('click', function(){ 
-      if ($(this).is('.prev') && imgNum !== 1) {
-        imgNum = imgNum - 1;
-        imgSrc = '../../images/detail/stealcut' + imgNum + '.jpg';
-        $('#modal_stealcut img').attr('src', imgSrc);
-        $(this).siblings('.prev_next').attr({'disabled': false}).css({'color': '#000'});
-      } 
-      else if ($(this).is('.next') && imgNum !== 23) {
-        imgNum = imgNum + 1;
-        imgSrc = '../../images/detail/stealcut' + imgNum + '.jpg';
-        $('#modal_stealcut img').attr('src', imgSrc);
-        $(this).siblings('.prev_next').attr({'disabled': false}).css({'color': '#000'});
-      }
-/*       else if (imgNum === 2 || imgNum === 22) {
-        $(this).attr({'disabled': true}).css({'color': '#7d7d7d'});
-      } 
-      방향 전환 될 수 있는 부분.. 어떻게 처리할 지 고민
-      */
-    });
+    $('html, body').css('overflowY', 'hidden');
 
     // 탭버튼 순환
+    $prevBtn.on('keydown', function(e){
+      if(e.shiftKey && e.keyCode === 9) {
+        e.preventDefault();
+        $clsBtn.focus();
+      }
+    });
+    $clsBtn.on('keydown', function(e){
+      if(!e.shiftKey && e.keyCode === 9) {
+        e.preventDefault();
+        $prevBtn.focus();
+      }                                         
+    });
 
-    // 스크롤 안되게 방지
+    // 다음버튼, 이전버튼
+    $prevBtn.on('click', function(){ 
+      imgNum = imgNum - 1;
+      imgSrc = '../../images/detail/stealcut' + imgNum + '.jpg';
+      $('#modal_stealcut > img').attr('src', imgSrc);
+      $(this).siblings('.prev_next').attr({'disabled': false}).css({'color': '#000'});
+      if (imgNum === 1) {
+        $(this).attr({'disabled': true}).css({'color': '#7d7d7d'});
+      } 
+    });
 
-    // 닫기 버튼 - 사진위치로 되돌아가야 함.
-    $closeBtn.on('click', function () {
+    $nextBtn.on('click', function(){ 
+      imgNum = imgNum + 1;
+      imgSrc = '../../images/detail/stealcut' + imgNum + '.jpg';
+      $('#modal_stealcut > img').attr('src', imgSrc);
+      $(this).siblings('.prev_next').attr({'disabled': false}).css({'color': '#000'});
+      if (imgNum === 23) {
+        $(this).attr({'disabled': true}).css({'color': '#7d7d7d'});
+      } 
+    });
+
+    // 닫기 버튼
+    $clsBtn.on('click', function () {
       $dim.stop().fadeOut('fast', function () {
         $(this).remove();
       });
       $mdCut.css('visibility', 'hidden').siblings().removeAttr('aria-hidden inert');
-      /* 여기에 다시 포커스 되돌아가게 */
+      $('#stealcut ul li img').eq(imgNum - 1).focus();
+      $('html, body').css('overflow', 'auto');
     });
 
     $dim.on('click', function () {
-      $closeBtn.trigger('click');
+      $clsBtn.trigger('click');
     });
     $(window).on('keydown', function (e) {
-      if (e.keyCode === 27) $closeBtn.click();
+      if (e.keyCode === 27) $clsBtn.click();
     });
-
   });
 
-
-  $('#stealcut ul li img').on('keydown', function () {
-    console.log();
+  $('#stealcut ul li img').on('keydown', function (e) {
+    if (e.keyCode === 13) $(this).trigger('click');
   });
-
 });
